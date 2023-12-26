e574038ce797283ae26a38cdedb3659747963485
axmed cali <axmdak4@gmail.com>
2023-12-26 15:18:31 +0300

    waan ka shaqeeyay

--- GithubCard/index.js
+++ GithubCard/index.js
@@ -52,5 +52,90 @@
 */
 
 const followersArray = [];
+const cardGit = document.querySelector(".cards")
+
+axios.get("https://api.github.com/users/axmed84")
+.then(response => {
+  // response.data.forEach(function(information) {
+    let myImage = response.data.avatar_url
+    let myName = response.data.name
+    let myUsername = response.data.login
+    let myLocation = response.data.location
+    let myProfile = response.data.html_url
+    let myFollowers = response.data.followers
+    let myFollowing = response.data.following
+    let myBio = response.data.Bio
+    cardGit.appendChild(githubUsers(myImage, myName, "login :"+myUsername, "Location: "+myLocation, "Profile: "+myProfile, "Followers:" +myFollowers, "Following: "+myFollowing, "Bio: "+myBio))
+  })
+axios.get("https://api.github.com/users/AbdirisakKhabir")
+.then(response => {
+  // response.data.forEach(function(information) {
+    let myImage = response.data.avatar_url
+    let myName = response.data.name
+    let myUsername = response.data.login
+    let myLocation = response.data.location
+    let myProfile = response.data.html_url
+    let myFollowers = response.data.followers
+    let myFollowing = response.data.following
+    let myBio = response.data.Bio
+    cardGit.appendChild(githubUsers(myImage, myName, myUsername, "Location: "+myLocation, "Profile: "+myProfile, "Followers:" +myFollowers, "Following: "+myFollowing, "Bio: "+myBio))
+  })
+axios.get("https://api.github.com/users/duraanali")
+.then(response => {
+  // response.data.forEach(function(information) {
+    let myImage = response.data.avatar_url
+    let myName = response.data.name
+    let myUsername = response.data.login
+    let myLocation = response.data.location
+    let myProfile = response.data.html_url
+    let myFollowers = response.data.followers
+    let myFollowing = response.data.following
+    let myBio = response.data.Bio
+    cardGit.appendChild(githubUsers(myImage, myName, "login :"+myUsername, "Location: "+myLocation, "Profile: "+myProfile, "Followers:" +myFollowers, "Following: "+myFollowing, "Bio: "+myBio))
+  })
+// }).catch(err => {
+//   console.log(err)
+// })
+
+function githubUsers(image, user, userName, location, profile, followers, following, bio){
+  // Step 1: creating elements
+  const card = document.createElement("div")
+  const userImg = document.createElement("img")
+  const infoCard = document.createElement("div")
+  const mainName = document.createElement("h3")
+  const auserName = document.createElement("p")
+  const userlocation = document.createElement("p")
+  const userProfile = document.createElement("p")
+  const userFallowers = document.createElement("p")
+  const userFallowing = document.createElement("p")
+  const userBio = document.createElement("p")
+  // Step 2: Connection
+  card.appendChild(userImg)
+  card.appendChild(infoCard)
+  infoCard.appendChild(mainName)
+  infoCard.appendChild(auserName)
+  infoCard.appendChild(userlocation)
+  infoCard.appendChild(userProfile)
+  infoCard.appendChild(userFallowers)
+  infoCard.appendChild(userFallowing)
+  infoCard.appendChild(userBio)
+  // step 3: adding CSS
+  card.classList.add("card")
+  userImg.classList.add("card", "img")
+  mainName.classList.add("name")
+  auserName.classList.add("username")
+  // step 4: add content
+  userImg.src = image
+  mainName.textContent = user
+  auserName.textContent = auserName
+  userlocation.textContent = location
+  userProfile.textContent = profile
+  userFallowers.textContent = followers
+  userFallowing.textContent = following
+
+return card
+
+}
+
 
 
